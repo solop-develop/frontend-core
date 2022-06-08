3adeb2f0ebd5bce64f8c6a3aab5257d9f182258d
elsiosanchez <elsiosanches@gmail.com>
2022-06-08 09:19:07 -0400

    Move reset cache to header

--- src/store/modules/ADempiere/system.js
+++ src/store/modules/ADempiere/system.js
@@ -19,7 +19,12 @@ import {
   requestLanguagesList
 } from '@/api/ADempiere/system-core.js'
 
+import {
+  requestRunProcess as runCache
+} from '@/api/ADempiere/process'
+
 // utils and helper methods
+import { processHeader } from '@/utils/ADempiere/constants/process'
 import { isEmptyValue } from '@/utils/ADempiere/valueUtils.js'
 import { convertDateFormat } from '@/utils/ADempiere/formatValue/dateFormat.js'
 
@@ -71,7 +76,16 @@ const system = {
             console.warn(`Error getting Languages List: ${error.message}. Code: ${error.code}.`)
           })
       })
+    },
+    runCache({ commit, dispatch }) {
+      runCache({
+        uuid: processHeader.resetCache
+      })
+        .then(() => {
+          location.reload()
+        })
     }
+
   },
 
   getters: {
