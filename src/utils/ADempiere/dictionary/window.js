cbe978db576423af0deeafa055750b0cb9bd9dbd
elsiosanchez <elsiosanches@gmail.com>
2022-10-25 10:13:22 -0400

    minimal changes

--- src/utils/ADempiere/dictionary/window.js
+++ src/utils/ADempiere/dictionary/window.js
@@ -82,7 +82,17 @@ export function isDisplayedField({ isDisplayed, displayLogic, isDisplayedFromLog
 /**
  * Default showed field from user
  */
-export function evaluateDefaultFieldShowed({ defaultValue, isMandatory, isShowedFromUser, isParent }) {
+export function evaluateDefaultFieldShowed({ containerUuid, parentUuid, columnName, defaultValue, isMandatory, isShowedFromUser, isParent }) {
+  console.log({ columnName })
+  // const { windowType } = store.getters.getStoredWindow(parentUuid)
+  // console.log({ windowType })
+  if (
+    columnName === 'DateOrdered' ||
+    columnName === 'M_Product_ID' ||
+    columnName === 'QtyEntered'
+  ) {
+    return true
+  }
   if (String(defaultValue).startsWith('@SQL=')) {
     return true
   }
