9f3ffee7d3c27902c7aa7b16e35059151beb7ece
EdwinBetanc0urt <EdwinBetanc0urt@outlook.com>
2023-05-22 10:34:28 -0400

    add jest enviroment dom.

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,14 @@ const theme = config.theme
 module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
   transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
-      'jest-transform-stub',
-    '^.+\\.jsx?$': 'babel-jest'
+    '^.+\\.vue$': '@vue/vue2-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.(js|jsx)?$': 'babel-jest'
   },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!vue-awesome)'
+    // "node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"
+  ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@theme/(.*)$': '<rootDir>/' + theme + '/$1'
@@ -17,6 +20,15 @@ module.exports = {
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    'browsers': [
+      'chrome',
+      'edge',
+      'firefox',
+      'safari'
+    ]
+  },
   collectCoverageFrom: ['src/utils/**/*.{js,vue}', '!src/utils/auth.js', '!src/utils/request.js', 'src/components/**/*.{js,vue}'],
   coverageDirectory: '<rootDir>/tests/unit/coverage',
   // 'collectCoverage': true,
