9911214b377da10121495fb45a0cd06aab0555a8
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2023-05-28 00:50:40 -0400

    Update match.js

--- src/lang/ADempiere/en/form/match.js
+++ src/lang/ADempiere/en/form/match.js
@@ -9,7 +9,7 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
@@ -25,7 +25,7 @@ const match = {
     assignFrom: 'Assign From',
     assignTo: 'Assign To',
     searchMode: 'Search Mode',
-    businessPartner: 'BusinessPartner',
+    businessPartner: 'Business Partner',
     product: 'Product',
     dateFrom: 'Date From',
     dateTo: 'Date To'
