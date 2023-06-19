a6e8fc2ea66826adde6f8b31728bd68d15833c88
Elsio Sanchez <elsiosanches@gmail.com>
2023-06-19 16:28:02 -0400

    minimal changes

--- src/utils/ADempiere/dataUtils.js
+++ src/utils/ADempiere/dataUtils.js
@@ -178,7 +178,7 @@ export const OPERATORS_FIELD_DATE = {
   componentPath: 'FieldDate',
   isRange: true,
   operatorsList: [
-    OPERATOR_BETWEEN,
+    OPERATOR_BETWEEN.operator,
     ...STANDARD_OPERATORS_LIST,
     ...RANGE_OPERATORS_LIST,
     ...MULTIPLE_OPERATORS_LIST
