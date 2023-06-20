fe9f447966ac79db82158cc01309a498b7945f8a
Elsio Sanchez <elsiosanches@gmail.com>
2023-06-20 13:47:29 -0400

    Add NOT_BETWEEN

--- src/utils/ADempiere/dataUtils.js
+++ src/utils/ADempiere/dataUtils.js
@@ -93,6 +93,14 @@ export const OPERATOR_BETWEEN = {
   symbol: '>-<'
 }
 
+export const OPERATOR_NOT_BETWEEN = {
+  operator: 'NOT_BETWEEN',
+  sqlOperators: [
+    'NOT_BETWEEN'
+  ],
+  symbol: '<->'
+}
+
 export const OPERATOR_NULL = {
   operator: 'NULL',
   sqlOperators: [
@@ -158,6 +166,7 @@ export const OPERATORS_LIST = [
   OPERATOR_LESS,
   OPERATOR_LESS_EQUAL,
   OPERATOR_BETWEEN,
+  OPERATOR_NOT_BETWEEN,
   OPERATOR_NOT_NULL,
   OPERATOR_NULL,
   OPERATOR_IN,
@@ -179,6 +188,7 @@ export const OPERATORS_FIELD_DATE = {
   isRange: true,
   operatorsList: [
     OPERATOR_BETWEEN.operator,
+    OPERATOR_NOT_BETWEEN.operator,
     ...STANDARD_OPERATORS_LIST,
     ...RANGE_OPERATORS_LIST,
     ...MULTIPLE_OPERATORS_LIST
