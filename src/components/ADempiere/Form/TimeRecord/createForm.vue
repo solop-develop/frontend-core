defa2e4fb56d77a6c0cb17e2294f43fa1f748eaa
EdwinBetanc0urt <EdwinBetanc0urt@outlook.com>
2023-12-27 16:55:25 -0400

    fix: Time record end points.

--- src/components/ADempiere/Form/TimeRecord/createForm.vue
+++ src/components/ADempiere/Form/TimeRecord/createForm.vue
@@ -1,25 +1,25 @@
 <!--
- ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
- Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
- Contributor(s): Elsio Sanchez elsiosanches@gmail.com https://github.com/elsiosanchez
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+  ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
+  Copyright (C) 2018-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
+  Contributor(s): Elsio Sanchez elsiosanches@gmail.com https://github.com/elsiosanchez
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program. If not, see <https:www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <https:www.gnu.org/licenses/>.
 -->
 
 <template>
   <el-form
     label-position="top"
-    class="time-record-create"
+    class="time-record-create form-min-label"
   >
     <el-row style="padding-bottom: 10px;">
       <el-col :span="sizeColumn">
@@ -143,7 +143,7 @@ import {
   requestCreateResource,
   requestlistIssues,
   requestlistProject
-} from '@/api/ADempiere/form/timeRecord.js'
+} from '@/api/ADempiere/form/timeRecord.ts'
 
 // Utils and Helper Methods
 import { showMessage } from '@/utils/ADempiere/notification'
@@ -229,7 +229,7 @@ export default defineComponent({
         projectId: proyect.value,
         name: name.value,
         description: description.value,
-        quantity: quantity.value,
+        quantity: quantity.value.toString(),
         date: dateValue.value
       })
         .then(response => {
