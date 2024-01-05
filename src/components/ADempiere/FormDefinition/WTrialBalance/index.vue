e5b8aee474a811ba96bf5af1a78c991c4965095d
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2024-01-05 08:13:29 -0400

    fix: `WTrialBalance` form translation. (#1826)

--- src/components/ADempiere/FormDefinition/WTrialBalance/index.vue
+++ src/components/ADempiere/FormDefinition/WTrialBalance/index.vue
@@ -1,19 +1,19 @@
 <!--
-ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
-Copyright (C) 2017-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
-Contributor(s): Elsio Sanchez Elsiosanches@gmail.com https://github.com/Elsiosanchez
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
+  Copyright (C) 2018-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
+  Contributor(s): Elsio Sanchez Elsiosanches@gmail.com https://github.com/Elsiosanchez
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <https:www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <https:www.gnu.org/licenses/>.
 -->
 
 <template>
@@ -227,8 +227,8 @@ import {
   // computed
 } from '@vue/composition-api'
 import lang from '@/lang'
-// API
 
+// API Request Methods
 import {
   listOrganizations,
   listBudgets,
@@ -246,12 +246,14 @@ import { isEmptyValue } from '@/utils/ADempiere/valueUtils.js'
 
 export default defineComponent({
   name: 'WTrialBalance',
+
   props: {
     metadata: {
       type: Object,
       default: () => {}
     }
   },
+
   setup(props) {
     /**
      * Ref
@@ -287,7 +289,7 @@ export default defineComponent({
         width: '120px'
       },
       {
-        label: lang.t('form.WTrialBalance.usuario'),
+        label: lang.t('form.WTrialBalance.user1'),
         columnName: 'user_list_name',
         width: '80px'
       },
@@ -530,7 +532,6 @@ export default defineComponent({
       // Computed
       validateBeforeSearch,
       // Methods
-      isEmptyValue,
       changeSelections,
       showListOrganization,
       showListBudgets,
