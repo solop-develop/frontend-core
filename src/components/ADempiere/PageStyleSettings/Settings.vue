9bc1ea21d7e20bd3cc972a8327200b4fb2727384
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2024-01-12 17:53:24 -0400

    Update Settings.vue

--- src/components/ADempiere/PageStyleSettings/Settings.vue
+++ src/components/ADempiere/PageStyleSettings/Settings.vue
@@ -1,20 +1,21 @@
 <!--
-ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
-Copyright (C) 2018-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
-Contributor(s): Elsio Sanchez elsiosanches@gmail.com https://github.com/elsiosanchez
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
+  Copyright (C) 2018-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
+  Contributor(s): Elsio Sanchez elsiosanches@gmail.com https://github.com/elsiosanchez
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <https:www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <https:www.gnu.org/licenses/>.
 -->
+
 <template>
   <el-card
     shadow="never"
@@ -150,24 +151,30 @@ along with this program. If not, see <https:www.gnu.org/licenses/>.
     </el-descriptions>
   </el-card>
 </template>
+
 <script>
 import {
   defineComponent,
   computed,
   ref
 } from '@vue/composition-api'
+
 import store from '@/store'
 import language from '@/lang'
-// components and mixins
+
+// Components and Mixins
 import ThemePicker from '@/components/ThemePicker'
+
 // Utils and Helper Methods
 import { isEmptyValue } from '@/utils/ADempiere/valueUtils.js'
 
 export default defineComponent({
   name: 'PageStyleSettings',
+
   components: {
     ThemePicker
   },
+
   setup() {
     // Ref
     const showSettings = ref(false)
