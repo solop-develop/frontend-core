3925d11828b7ca3610535b9ac5d18ec2a54e610c
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2024-02-16 17:20:50 -0400

    fix: Docker image set api env. (#1929)
    * fix: Docker image set api env.
    
    * fix port.

--- build/production-alpine.Dockerfile
+++ build/production-alpine.Dockerfile
@@ -6,8 +6,8 @@ LABEL maintainer="Elsiosanches@gmail.com; EdwinBetanc0urt@outlook.com;" \
 
 # Init ENV with default values
 ENV PUBLIC_PATH="/" \
-	API_URL="http://localhost:8080/api/" \
-	TASK_MANAGER_URL="http://localhost:8080/v1" \
+	API_URL="http://localhost/api/" \
+	TASK_MANAGER_URL="http://localhost/v1" \
 	TZ="America/Caracas"
 
 
--- build/start.sh
+++ build/start.sh
@@ -11,7 +11,7 @@ cd /usr/share/nginx/html/static/js
 
 
 # Set API Proxy connection
-find -name 'app.*.js' -exec sed -i "s|http://localhost:8080/api/|$API_URL|g" {} \;
+find -name 'app.*.js' -exec sed -i "s|http://localhost/api/|$API_URL|g" {} \;
 
 # Set Task Manager connection
 find -name 'app.*.js' -exec sed -i "s|http://localhost:8080/v1|$TASK_MANAGER_URL|g" {} \;
