b6100254ea776b3ef6ee25ecb066d16f1e4c27af
EdwinBetanc0urt <EdwinBetanc0urt@outlook.com>
2024-03-27 18:39:29 -0400

    fix: Generate report XLSX.

--- src/components/ADempiere/FileRender/HtmlFile.vue
+++ src/components/ADempiere/FileRender/HtmlFile.vue
@@ -1,17 +1,19 @@
 <!--
- ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
- Copyright (C) 2018-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
- Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com https://github.com/EdwinBetanc0urt
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
- You should have received a copy of the GNU General Public License
- along with this program. If not, see <https:www.gnu.org/licenses/>.
+  ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
+  Copyright (C) 2018-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
+  Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com https://github.com/EdwinBetanc0urt
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <https:www.gnu.org/licenses/>.
 -->
 
 <template>
@@ -98,6 +100,7 @@ export default defineComponent({
       required: true
     }
   },
+
   setup(props) {
     const excelData = ref({})
 
@@ -147,6 +150,7 @@ export default defineComponent({
         reader.readAsArrayBuffer(blobFile)
       })
     }
+
     function rowClassName(params) {
       return 'success-row'
     }
@@ -159,7 +163,7 @@ export default defineComponent({
     }
 
     function dataCell(row) {
-      if (row === 'Y' || row === 'N') {
+      if (String(row).trim() === 'Y' || String(row).trim() === 'N') {
         return convertBooleanToTranslationLang(row)
       } else if (typeof row === 'string' && row.includes('&#931')) {
         return 'Σ'
