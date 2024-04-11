a1df631ff660dbc028416de155dd61978117e8a8
Ricargame <ricardofenomeno13@gmail.com>
2024-04-11 10:16:16 -0400

    bugfix: Advanced query translation

--- src/lang/ADempiere/en/window.js
+++ src/lang/ADempiere/en/window.js
@@ -18,8 +18,8 @@
 
 const window = {
   advancedQuery: {
-    title: 'Consulta Avanzada',
-    help: 'Realiza una búsqueda haciendo uso de rangos de valores, selección múltiple de valores, y operadores de comparación'
+    title: 'Advanced Query',
+    help: 'Perform a search using ranges of values, multiple selection of values, and comparison operators'
   },
   newRecord: 'New Record',
   copyRecord: 'Copy Record',
@@ -62,7 +62,8 @@ const window = {
       recordID: 'Record ID',
       recordUUID: 'Record UUID',
       tab: 'Tab',
-      tableName: 'Table Name'
+      tableName: 'Table Name',
+      allChanges: 'See All Changes'
     },
     notices: {
       user: 'User'
