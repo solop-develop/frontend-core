f358efea5a26bc997b3f82088a97336c348ef7aa
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2024-05-23 18:11:27 -0400

    Update Issues.js

--- src/utils/ADempiere/dictionary/form/Issues.js
+++ src/utils/ADempiere/dictionary/form/Issues.js
@@ -17,7 +17,9 @@
  */
 
 export const REQUEST_WINDOW_UUID = 'a5216792-fb40-11e8-a479-7a0060f0aa01'
-export const ISSUES_ID = 233
+
+export const REQUEST_ALL_WINDOW_ID = 233
+
 export function dueTypeColor(issue) {
   const { due_type } = issue
   const { value } = due_type
