c39ff81e1b49b3e690498502a06143e40b9bfdcd
Elsio Sanchez <elsiosanches@gmail.com>
2024-05-24 17:42:07 -0400

    Feat: Language

---
+++ src/utils/ADempiere/dictionary/language.ts
@@ -0,0 +1,33 @@
+/**
+ * ADempiere-Vue (Frontend) for ADempiere ERP & CRM Smart Business Solution
+ * Copyright (C) 2018-Present E.R.P. Consultores y Asociados, C.A. www.erpya.com
+ * Contributor(s): Elsio Sanchez elsiosanches@gmail.com https://github.com/ElsioSanchez
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+export const DEFAULT_LANGUAGE_LIST = [
+  { language_iso: 'en', language_name: 'English' },
+  { language_iso: 'es', language_name: 'Español' },
+  { language_iso: 'fr', language_name: 'Français' },
+  { language_iso: 'de', language_name: 'Deutsch' },
+  { language_iso: 'it', language_name: 'Italiano' },
+  { language_iso: 'ja', language_name: '日本語' },
+  { language_iso: 'ko', language_name: '한국어' },
+  { language_iso: 'nl', language_name: 'Nederlands' },
+  { language_iso: 'pl', language_name: 'Polski' },
+  { language_iso: 'pt', language_name: 'Português' },
+  { language_iso: 'ru', language_name: 'Русский' },
+  { language_iso: 'tr', language_name: 'Türkçe' },
+  { language_iso: 'zh', language_name: '中文' }
+]
