f68d477540c61dda222fed5118dd48f187b76627
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2024-06-04 15:00:01 -0400

    Update get-page-favicon.js

--- src/utils/get-page-favicon.js
+++ src/utils/get-page-favicon.js
@@ -3,7 +3,6 @@ import store from '@/store'
 export default function getPageFavicon() {
   const { logoUrl } = store.getters['user/getSystem']
   let link = document.querySelector("link[rel~='icon']")
-  console.log({ link })
   if (!link) {
     link = document.createElement('link')
     link.rel = 'icon'
