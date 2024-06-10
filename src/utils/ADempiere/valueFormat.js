f67adc5907e50c202822e332cb6c2496f67983ab
Elsio Sanchez <elsiosanches@gmail.com>
2024-06-10 16:24:39 -0400

    #2326

--- src/utils/ADempiere/valueFormat.js
+++ src/utils/ADempiere/valueFormat.js
@@ -97,7 +97,11 @@ export function formatField({
       formattedValue = formatDateTemp({
         value,
         isTime: false,
-        format: optionalFormat
+        format: getDateFormat({
+          format: 'YYYY-MM-DD',
+          isTime: false,
+          isDate: true
+        })
       })
       break
 
