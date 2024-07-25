24ab00b532de436d2efd760079e8d594eeec11c4
Ricargame <ricardofenomeno13@gmail.com>
2024-07-25 09:13:42 -0400

    Fix: Color Red In Popover

--- src/views/ADempiere/ReportViewerEngine/infoReport.vue
+++ src/views/ADempiere/ReportViewerEngine/infoReport.vue
@@ -3,37 +3,37 @@
     <el-col :span="18" style="width: 100%;">
       <el-descriptions :column="3" :label-style="{'font-weight': 'bold'}" class="contentModalReport">
         <el-descriptions-item :label="$t('report.reportEnginer.max')" class="small-text">
-          <span :style="{ 'color': data.maximum_value.display_value < 0 ? 'red' : '' }">
+          <span :style="{ 'color':isNegativeValue(data.maximum_value.display_value) ? 'red' : ''}">
             {{ data.maximum_value.display_value }}
           </span>
         </el-descriptions-item>
         <el-descriptions-item :label="$t('report.reportEnginer.summation')" class="small-text">
-          <span :style="{ 'color': data.sum_value.display_value < 0 ? 'red' : '' }">
+          <span :style="{ 'color':isNegativeValue(data.sum_value.display_value) ? 'red' : ''}">
             {{ data.sum_value.display_value }}
           </span>
         </el-descriptions-item>
         <el-descriptions-item :label="$t('report.reportEnginer.standardDeviation')" class="small-text">
-          <span :style="{ 'color': data.deviation_value.display_value < 0 ? 'red' : '' }">
+          <span :style="{ 'color':isNegativeValue(data.deviation_value.display_value) ? 'red' : ''}">
             {{ data.deviation_value.display_value }}
           </span>
         </el-descriptions-item>
         <el-descriptions-item :label="$t('report.reportEnginer.average')" class="small-text">
-          <span :style="{ 'color': data.mean_value.display_value < 0 ? 'red' : '' }">
+          <span :style="{ 'color':isNegativeValue(data.mean_value.display_value) ? 'red' : ''}">
             {{ data.mean_value.display_value }}
           </span>
         </el-descriptions-item>
         <el-descriptions-item :label="$t('report.reportEnginer.Count')" class="small-text">
-          <span :style="{ 'color': data.count_value.display_value < 0 ? 'red' : '' }">
+          <span :style="{ 'color':isNegativeValue(data.count_value.display_value) ? 'red' : ''}">
             {{ data.count_value.display_value }}
           </span>
         </el-descriptions-item>
         <el-descriptions-item :label="$t('report.reportEnginer.variance')" class="small-text">
-          <span :style="{ 'color': data.variance_value.display_value < 0 ? 'red' : '' }">
+          <span :style="{ 'color':isNegativeValue(data.variance_value.display_value) ? 'red' : ''}">
             {{ data.variance_value.display_value }}
           </span>
         </el-descriptions-item>
         <el-descriptions-item :label="$t('report.reportEnginer.min')" class="small-text">
-          <span :style="{ 'color': data.minimum_value.display_value < 0 ? 'red' : '' }">
+          <span :style="{ 'color':isNegativeValue(data.minimum_value.display_value) ? 'red' : ''}">
             {{ data.minimum_value.display_value }}
           </span>
         </el-descriptions-item>
@@ -53,6 +53,15 @@ export default defineComponent({
     data: {
       type: Object,
       required: true
+    }
+  },
+  setup() {
+    function isNegativeValue(value) {
+      const parsedValue = parseFloat(value)
+      return !isNaN(parsedValue) && parsedValue < 0
+    }
+    return {
+      isNegativeValue
     }
   }
 })
