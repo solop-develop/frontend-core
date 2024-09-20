5ec57affbfd308493979fc83e62470563e5e1e16
Ricargame <ricardofenomeno13@gmail.com>
2024-09-20 18:23:53 -0400

    Fix: Report Enginer Version

--- src/lang/ADempiere/en/index.js
+++ src/lang/ADempiere/en/index.js
@@ -238,7 +238,7 @@ export default {
       deploymentName: 'Deployment Name',
       dictionaryVersion: 'Dictionary RS version',
       s3Version: 'S3 Gateway RS Version',
-      reportEnginer: 'Report Enginer Version'
+      reportEnginer: 'Report Engine Version'
     }
   },
   window,
